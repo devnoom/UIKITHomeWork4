a7cfd147de8920cc5f7587e28867b9c9def856d3
devnoom <archilmenabde@yahoo.com>
2024-04-10 15:03:20 +0400

    Initial Commit

---
+++ UIKITHomeWork4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKITHomeWork4
+//
+//  Created by MacBook Air on 10.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
